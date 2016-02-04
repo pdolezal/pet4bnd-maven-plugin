c7feeeac730e7c8e27aee34da4f6a3b0a8762e4a
Petr Doležal <petr.dolezal@matfyz.cz>
2016-02-04 21:54:18 +0100

    Fork version 1.x

---
+++ src/main/java/net/yetamine/pet4bnd/Main.java
@@ -0,0 +1,17 @@
+package net.yetamine.pet4bnd;
+
+/**
+ * Implementation of the command line interface of the tool.
+ */
+public final class Main {
+
+    /**
+     * Launches the command line version of the tool.
+     *
+     * @param args
+     *            the command line arguments. It must not be {@code null}.
+     */
+    public static void main(String... args) {
+        // TODO Auto-generated method stub
+    }
+}
---
+++ src/main/java/net/yetamine/pet4bnd/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * <i>Package exports tracker for bnd</i> implementation.
+ */
+package net.yetamine.pet4bnd;
