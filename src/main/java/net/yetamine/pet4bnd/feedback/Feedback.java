1334c22dbb74bcfaf869342583924ea83983207e
Petr Doležal <petr.dolezal@matfyz.cz>
2016-04-01 00:17:09 +0200

    Squash no-feedback implementation

--- src/main/java/net/yetamine/pet4bnd/feedback/Feedback.java
+++ src/main/java/net/yetamine/pet4bnd/feedback/Feedback.java
@@ -98,3 +98,35 @@ public interface Feedback {
         return NoFeedback.INSTANCE;
     }
 }
+
+/**
+ * A nothing-doing implementation of the parser feedback interface.
+ */
+enum NoFeedback implements Feedback {
+
+    /** Sole instance of this feedback. */
+    INSTANCE;
+
+    /**
+     * @see net.yetamine.pet4bnd.feedback.Feedback#fail(java.lang.String,
+     *      java.lang.Throwable)
+     */
+    public void fail(String message, Throwable t) {
+        // Do nothing
+    }
+
+    /**
+     * @see net.yetamine.pet4bnd.feedback.Feedback#warn(java.lang.String,
+     *      java.lang.Throwable)
+     */
+    public void warn(String message, Throwable t) {
+        // Do nothing
+    }
+
+    /**
+     * @see net.yetamine.pet4bnd.feedback.Feedback#info(java.lang.String)
+     */
+    public void info(String message) {
+        // Do nothing
+    }
+}
